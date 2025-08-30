3a49f1549765253e873fa8121c1c58afaaefd473
Shubham <shubhamchouh@gmail.com>
2025-08-30 18:31:00 +0530

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://loan-management-system-backend-production-1bd1.up.railway.app/api',
   appName: 'Loan Management System',
   version: '1.0.0'
 };
