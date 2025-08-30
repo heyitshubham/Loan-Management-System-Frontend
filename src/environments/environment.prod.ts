1c912d01abc3eda4e98df1049faadcd5aa1f6727
Shubham <shubhamchouh@gmail.com>
2025-08-30 18:07:35 +0530

    API_URL vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url.com/api',
+  apiUrl: process.env['API_URL'] || 'https://loan-management-system-backend-production-1bd1.up.railway.app/api',
   appName: 'Loan Management System',
   version: '1.0.0'
 };
